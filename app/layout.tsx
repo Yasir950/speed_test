27bcfb8080b36c818db20e2886a424d5a294bc6e
Muhammad Yasir Abbas <368yasir@gmail.com>
2026-08-13 19:11:02 +0500

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -109,6 +109,11 @@ export default function RootLayout({
     >
       <head>
         <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9086432794955952"
+          crossorigin="anonymous"
+        ></script>
+        <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
             __html: JSON.stringify(softwareAppJsonLd),
